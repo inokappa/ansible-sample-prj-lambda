c72b3e77e9c2839ddf4119da1813e62c426169cc
inokappa <inokara@gmail.com>
2017-04-27 08:13:52 +0900

    first commit

--- functions/demo/sample.py
+++ functions/demo/sample.py
@@ -6,7 +6,3 @@ sys.path.append("./site-packages")
 
 def sample_handler(event, context):
     print(u'Ansible 徹底入門、絶賛販売中!!')
-    message = 'Ansible 徹底入門、絶賛販売中!!'
-    return { 
-        'message' : message
-    }
